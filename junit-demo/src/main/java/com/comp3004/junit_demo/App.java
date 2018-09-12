8347d061cee764c58fb96d8607057034b833bb40
KartikVashisth97 <kartikvashisth675@gmail.com>
2018-09-12 14:58:45 -0400

    init: maven project

---
+++ junit-demo/src/main/java/com/comp3004/junit_demo/App.java
@@ -0,0 +1,13 @@
+package com.comp3004.junit_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
